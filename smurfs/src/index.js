3e1982fb338fd5d62005abe0be0ad0dd543d40ae
NaJ <naj10000@live.com>
2020-09-27 21:41:59 -0400

    setup

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,5 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleWare } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(Reducer, applyMiddleWare(thunk))
+
+
+ReactDOM.render(<Provider store={store}>
+    <App />
+</Provider>, document.getElementById("root"));
